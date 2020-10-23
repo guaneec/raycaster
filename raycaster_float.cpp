d541b804f8fb1d38e677747297776e605ebb1246
Dan <guaneec@gmail.com>
2020-10-23 16:51:00 +0800

    Fix rendering of close walls

--- raycaster_float.cpp
+++ raycaster_float.cpp
@@ -149,12 +149,13 @@ void RayCasterFloat::Trace(uint16_t screenX,
     *textureStep = 0;
     if (distance > 0) {
         *screenY = INV_FACTOR / distance;
-        auto txs = (*screenY * 2.0f);
+        auto txs = (INV_FACTOR / distance * 2.0f);
         if (txs != 0) {
             *textureStep = (256 / txs) * 256;
             if (txs > SCREEN_HEIGHT) {
                 auto wallHeight = (txs - SCREEN_HEIGHT) / 2;
                 *textureY = wallHeight * (256 / txs) * 256;
+                *screenY = SCREEN_HEIGHT / 2;
             }
         }
     } else {
